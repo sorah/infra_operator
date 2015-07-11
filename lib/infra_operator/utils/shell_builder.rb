c9c96f70d1b5f310b007896d9880816d80d7a7f3
Shota Fukumori (sora_h) <her@sorah.jp>
2015-07-12 07:50:29 +0900

    Add Utils::ShellBuilder

---
+++ lib/infra_operator/utils/shell_builder.rb
@@ -0,0 +1,163 @@
+require 'shellwords'
+
+module InfraOperator
+  module Utils
+    class ShellBuilder
+      def initialize(&block)
+        @block = block
+      end
+
+      def to_s
+        Context.new(&@block).to_s
+      end
+
+      class Context
+        def initialize(&block)
+          @block = block
+          @list = []
+          instance_eval &@block
+        end
+
+        attr_reader :list
+
+        def to_s
+          @list.map(&:to_s).join("\n")
+        end
+
+        def var(*args)
+          @list << Variable.new(*args)
+        end
+
+        def export(*args)
+          @list << Variable.new(*args).export
+        end
+
+        def cd(*args)
+          @list << Chdir.new(*args)
+        end
+
+        def run(*args)
+          @list << Run.new(*args)
+        end
+
+        def pipe(&block)
+          @list << Context.new(&block).list.map(&:to_s).join(' | ')
+        end
+
+        def subshell(&block)
+          @list << [
+            '(',
+            *Context.new(&block).list.map(&:to_s),
+            ')',
+          ].join("\n")
+        end
+
+        def with_and(&block)
+          @list << Context.new(&block).list.map(&:to_s).join(' && ')
+        end
+
+        def with_or(&block)
+          @list << Context.new(&block).list.map(&:to_s).join(' || ')
+        end
+      end
+
+      class Run
+        def initialize(*args)
+          @args = args
+          @options = args.last.kind_of?(Hash) ? args.pop : {}
+          @args.flatten!
+        end
+
+        def command
+          @args.map { |_| Shellwords.escape(_) }
+        end
+
+        def redirect
+          redirects = @options.map do |k,v|
+            next unless k.kind_of?(Integer) || k == :out || k == :err || k == :in
+            make_redirection(k, v)
+          end.compact
+
+          redirects.empty? ? nil : redirects.join(' ')
+        end
+
+        def to_s
+          [*command, *redirect].join(" ")
+        end
+
+        private
+
+        def make_redirection(k, v)
+          fd = {:in => '', :out => '', :err => 2}[k] || k
+
+          direction = k == :in ? '<' : '>'
+
+          if v.kind_of?(Array)
+            v, orig_v = v.dup, v
+            if v.first == :read
+              v.shift
+              direction = '<'
+            end
+
+            if v.first == :rw
+              v.shift
+              direction = '<>'
+            end
+
+            if v.first == :append
+              v.shift
+              direction = '>>'
+            end
+
+            unless v.size == 1
+              raise ArgumentError, "invalid redirect (#{k.inspect} => #{orig_v.inspect})"
+            end
+
+            v = v.first
+          end
+
+          if v.kind_of?(Integer)
+            dest = "&#{v}"
+          else
+            dest = Shellwords.escape(v.to_s)
+          end
+
+          [fd, direction, dest].join
+        end
+      end
+
+      class Chdir
+        def initialize(destination)
+          @destination = destination
+        end
+
+        def to_s
+          "cd #{Shellwords.escape(@destination)}"
+        end
+      end
+
+      class Variable
+        def initialize(variables = {})
+          @variables = variables
+          @export = variables.delete(:export)
+        end
+
+        def export
+          self.class.new(@variables.merge(:export => true))
+        end
+
+        def export?
+          @export
+        end
+
+        def escaped_variable_definitions
+          @variables.map { |k, v| "#{@export ? 'export ' : nil}#{Shellwords.shellescape(k)}=#{Shellwords.shellescape(v)}" }
+        end
+
+        def to_s
+          escaped_variable_definitions.join("\n")
+        end
+      end
+    end
+  end
+end
---
+++ spec/utils/shell_builder_spec.rb
@@ -0,0 +1,194 @@
+require 'spec_helper'
+require 'infra_operator/utils/shell_builder'
+
+RSpec.describe InfraOperator::Utils::ShellBuilder do
+  def build(&block)
+    described_class.new(&block).to_s
+  end
+
+  describe "var" do
+    subject { build { var('foo' => 'bar baz', 'hoge' => 'fuga') }.lines.map(&:chomp) }
+
+    it { is_expected.to include("foo=bar\\ baz") }
+    it { is_expected.to include("hoge=fuga") }
+  end
+
+  describe "export" do
+    subject { build { export('foo' => 'bar baz', 'hoge' => 'fuga') }.lines.map(&:chomp) }
+
+    it { is_expected.to include("export foo=bar\\ baz") }
+    it { is_expected.to include("export hoge=fuga") }
+  end
+
+  describe "cd" do
+    context "simple" do
+      subject { build { cd "/tmp" } }
+      it { is_expected.to eq "cd /tmp" }
+    end
+
+    context "with spaces" do
+      subject { build { cd "/tmp tmp" } }
+      it { is_expected.to eq "cd /tmp\\ tmp" }
+    end
+  end
+
+  describe "run" do
+    context "simple" do
+      subject { build { run 'foo', 'bar', '1 2' } }
+      it { is_expected.to eq "foo bar 1\\ 2" }
+    end
+
+    context "array" do
+      subject { build { run %w(foo bar) } }
+      it { is_expected.to eq "foo bar" }
+    end
+
+    context "array and values" do
+      subject { build { run %w(foo bar), 'baz' } }
+      it { is_expected.to eq "foo bar baz" }
+    end
+
+    context "redirection" do
+      subject(:script) do
+        build do
+          run(
+            'foo',
+            {
+              :in => 'in.txt',
+              :out => 'out.txt',
+              :err => 'err.txt',
+              10 => '10.txt',
+              21 => [:read, '21.txt'],
+              22 => [:rw, '22.txt'],
+              23 => [:append, '23.txt'],
+              31 => 91,
+              32 => [:read, 92],
+              33 => [:rw, 93],
+              40 => 'foo bar.txt',
+            }
+          )
+        end
+      end
+
+      subject(:splitted_subject) { script.shellsplit }
+
+      specify {
+        expect(splitted_subject.size).to eq 12
+        expect(splitted_subject.first).to eq 'foo'
+        expect(splitted_subject).to include('<in.txt')
+        expect(splitted_subject).to include('>out.txt')
+        expect(splitted_subject).to include('2>err.txt')
+        expect(splitted_subject).to include('10>10.txt')
+        expect(splitted_subject).to include('21<21.txt')
+        expect(splitted_subject).to include('22<>22.txt')
+        expect(splitted_subject).to include('23>>23.txt')
+        expect(splitted_subject).to include('31>&91')
+        expect(splitted_subject).to include('32<&92')
+        expect(splitted_subject).to include('33<>&93')
+        expect(splitted_subject).to include("40>foo bar.txt")
+        expect(script).to include("40>foo\\ bar.txt")
+      }
+    end
+  end
+
+  describe "pipe" do
+    subject do
+      build do
+        pipe do
+          run 'a'
+          run 'b'
+        end
+      end
+    end
+
+    it { is_expected.to eq "a | b" }
+  end
+
+  describe "subshell" do
+    subject do
+      build do
+        subshell do
+          run 'a'
+          run 'b'
+        end
+      end
+    end
+
+    it { is_expected.to eq <<-EOF.chomp }
+(
+a
+b
+)
+    EOF
+  end
+
+  describe "with_and" do
+    subject do
+      build do
+        with_and do
+          run 'a'
+          run 'b'
+        end
+      end
+    end
+
+    it { is_expected.to eq "a && b" }
+  end
+
+  describe "with_or" do
+    subject do
+      build do
+        with_or do
+          run 'a'
+          run 'b'
+        end
+      end
+    end
+
+    it { is_expected.to eq "a || b" }
+  end
+
+  context  do
+    subject do
+      build do
+        cd '/tmp'
+        export 'foo' => 'bar'
+
+        run 'a', 'hello', 'world'
+
+        subshell do
+          run 'b.a'
+          run 'b.b'
+
+          pipe do
+            run 'b.c.a'
+            run 'b.c.b'
+          end
+        end
+
+        with_and do
+          run 'c.a'
+          run 'c.b'
+          with_or do
+            run 'c.c.a'
+            run 'c.c.b'
+          end
+        end
+      end
+    end
+
+    specify "complex one" do
+      expect(subject).to eq <<-EOF.chomp
+cd /tmp
+export foo=bar
+a hello\ world
+(
+b.a
+b.b
+b.c.a | b.c.b
+)
+c.a && c.b && c.c.a || c.c.b
+      EOF
+    end
+  end
+end
